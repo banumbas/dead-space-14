2db5f21ba6074de0352748b4b4c6aa6bc968c617
Алексей Герасимов <gerasimovav@althub.pro>
2026-07-12 22:08:58 +0300

    fix

--- Content.Server/DeadSpace/Guardian/GuardianSystem.AlertRelay.cs
+++ Content.Server/DeadSpace/Guardian/GuardianSystem.AlertRelay.cs
@@ -1,24 +1,30 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
 using Content.Shared.DeadSpace.Alerts;
 
 namespace Content.Server.Guardian;
 
 public sealed partial class GuardianSystem
 {
+    [Dependency] private readonly HealthAlertRelaySystem _healthAlertRelay = default!;
+
     partial void OnGuardianCreated(EntityUid guardian, EntityUid host)
     {
-        if (TryComp<HealthAlertRelayComponent>(guardian, out var relayComp))
-        {
-            relayComp.LinkedEntity = host;
-            Dirty(guardian, relayComp);
-        }
+        SetHealthAlertRelay(guardian, host);
     }
 
     private void OnGuardianStartup(EntityUid uid, GuardianComponent component, ComponentStartup args)
     {
-        if (TryComp<HealthAlertRelayComponent>(uid, out var relay))
-        {
-            relay.LinkedEntity = component.Host;
-            Dirty(uid, relay);
-        }
+        SetHealthAlertRelay(uid, component.Host);
+    }
+
+    private void SetHealthAlertRelay(EntityUid guardian, EntityUid? host)
+    {
+        if (!TryComp<HealthAlertRelayComponent>(guardian, out var relay))
+            return;
+
+        relay.LinkedEntity = host;
+        Dirty(guardian, relay);
+        _healthAlertRelay.UpdateRelay(guardian, relay);
     }
 }
--- Content.Shared/DeadSpace/Alerts/HealthAlertRelayComponent.cs
+++ Content.Shared/DeadSpace/Alerts/HealthAlertRelayComponent.cs
@@ -1,3 +1,5 @@
+// Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
+
 using Robust.Shared.GameStates;
 
 namespace Content.Shared.DeadSpace.Alerts;
@@ -8,6 +10,8 @@ namespace Content.Shared.DeadSpace.Alerts;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 public sealed partial class HealthAlertRelayComponent : Component
 {
+    public override bool SendOnlyToOwner => true;
+
     /// <summary>
     /// The entity whose health alerts are being relayed to this entity.
     /// </summary>
